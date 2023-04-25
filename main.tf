9d68fa5e3fedef4a044e72f0687d76c0fc3a179d
farooq <farooq.sbey@gmail.com>
2023-04-25 16:18:24 +0100

    Final formatting

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
-  public_cidr  = ["173.152.1.0/24", "173.152.2.0/24"]
-  private_cidr = ["173.152.3.0/24", "173.152.4.0/24"]
-  A_Z          = ["us-east-1a", "us-east-1b"]
+  public_cidr         = ["173.152.1.0/24", "173.152.2.0/24"]
+  private_cidr        = ["173.152.3.0/24", "173.152.4.0/24"]
+  A_Z                 = ["us-east-1a", "us-east-1b"]
   private_route_table = ["173.152.3.0/24", "173.152.4.0/24"]
 }
 
@@ -66,14 +66,14 @@ resource "aws_route_table" "public" {
 
 # Associate the public subnets with the public route table
 resource "aws_route_table_association" "public" {
-  count = length(local.public_cidr)
+  count          = length(local.public_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 # Create private route tables
 resource "aws_route_table" "private" {
-  count = length(local.private_cidr)
+  count  = length(local.private_cidr)
   vpc_id = aws_vpc.fsb_vpc.id
 
   #ROUTE
@@ -82,38 +82,38 @@ resource "aws_route_table" "private" {
     nat_gateway_id = aws_nat_gateway.nat_gateways[count.index].id
   }
   tags = {
-  Name = "private${count.index}"
+    Name = "private${count.index}"
   }
 }
 
 # Associate private subnets with the private rt's
 resource "aws_route_table_association" "private_subnet_associations" {
-  count = length(local.private_cidr)
+  count          = length(local.private_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 
   tags = {
-  Name = "$(var.env_code)-private_association${count.index}"
+    Name = "$(var.env_code)-private_association${count.index}"
   }
 }
 
 # Create 2 NAT Gateways & Associate with public subnets 1&2
 resource "aws_nat_gateway" "nat_gateways" {
-  count = length(local.public_cidr)
+  count         = length(local.public_cidr)
   allocation_id = aws_eip.nat_gateway[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-  Name = "$(var.env_code)-Nat_GW${count.index}"
+    Name = "$(var.env_code)-Nat_GW${count.index}"
   }
 }
 
 # EIPs for the NAT Gateways
 resource "aws_eip" "nat_gateway" {
   count = length(local.public_cidr)
-  vpc = true
+  vpc   = true
 
   tags = {
-  Name = "$(var.env_code)-Nat_GW_EIP${count.index}"
+    Name = "$(var.env_code)-Nat_GW_EIP${count.index}"
   }
 }
