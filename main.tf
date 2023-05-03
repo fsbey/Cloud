3b3a5727b5878d0202018356600fa54de200e023
farooq <farooq.sbey@gmail.com>
2023-05-03 13:39:38 +0100

    Final state lock changes

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ locals {
 
 terraform {
   backend "s3" {
-    bucket = "tfremotestatefsb"
-    key    = "fsb.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "fsb_dynamo_dbtable"
+    bucket         = "tfremotestatefsb"
+    key            = "fsb.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fsb_dynamo_db"
   }
 }
 
