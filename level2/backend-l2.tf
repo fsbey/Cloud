f6d68eab8b81032c46e57071870b2b880be63929
farooq <farooq.sbey@gmail.com>
2023-05-05 17:18:54 +0100

    Changing bucket name in level2

--- level2/backend-l2.tf
+++ level2/backend-l2.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = data.aws_s3_bucket.example_bucket.id 
-    key            = "fsb.tfstate"
+    bucket         = "tfremotestatefsb"
+    key            = "fsbL2.tfstate"
     region         = "us-east-1"
     dynamodb_table = data.aws_dynamodb_table.example_table.name
 }
