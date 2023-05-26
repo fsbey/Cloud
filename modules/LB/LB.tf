82ce3a36752f655d8e1a3db54fcc5243b4d2db1c
farooq <farooq.sbey@gmail.com>
2023-05-26 15:57:43 +0100

    Amendments to acm.tf

--- modules/LB/LB.tf
+++ modules/LB/LB.tf
@@ -20,8 +20,8 @@ resource "aws_security_group" "alb_sg" {
 
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -40,8 +40,6 @@ resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
   certificate_arn = aws_acm_certificate.main.arn
 
   default_action {
@@ -54,7 +52,7 @@ resource "aws_lb_listener" "alb_listener" {
 #TG Creation1
 resource "aws_lb_target_group" "TG" {
   name_prefix = "FsbTG"
-  port        = 443
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
 
@@ -62,7 +60,7 @@ resource "aws_lb_target_group" "TG" {
     interval            = 30
     path                = "/"
     port                = "traffic-port"
-    timeout             = 5
+    timeout             = 20
     healthy_threshold   = 5
     unhealthy_threshold = 2
     matcher             = 200
